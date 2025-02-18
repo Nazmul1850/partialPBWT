274920744b1a00dfc8b2af811badac79f983fa48
Nazmul Hasan <46302179+Nazmul1850@users.noreply.github.com>
2025-02-18 11:52:33 +0600

    algo 1 initial

---
+++ PBWT.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class PBWT {
+    
+    // Algorithm 1: Build Prefix Array
+    public static void buildPrefixArray(int[][] sequences, int[][] prefixArray) {
+        int M = sequences.length;
+        int N = sequences[0].length;
+
+        for (int k = 0; k < N; k++) {
+            Integer[] indices = new Integer[M];
+            for (int i = 0; i < M; i++) {
+                indices[i] = i;
+            }
+            final int currentK = k;
+            Arrays.sort(indices, (a, b) -> {
+                for (int j = currentK; j >= 0; j--) {
+                    if (sequences[a][j] != sequences[b][j]) {
+                        return sequences[a][j] - sequences[b][j];
+                    }
+                }
+                return 0;
+            });
+            for (int i = 0; i < M; i++) {
+                prefixArray[k][i] = indices[i];
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        int[][] sequences = {
+            {0, 1, 0, 1, 1},
+            {1, 0, 1, 1, 0},
+            {0, 0, 1, 0, 1},
+            {1, 1, 0, 0, 0}
+        };
+        int M = sequences.length;
+        int N = sequences[0].length;
+        int[][] prefixArray = new int[N][M];
+        buildPrefixArray(sequences, prefixArray);
+
+    }
+}
